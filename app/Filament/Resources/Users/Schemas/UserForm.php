bd1f48cc5c62357a5ae5fd49db29cf5af5ddf70e
andyov17 <andrew.ovens@hotmail.com>
2025-10-02 11:58:13 +0100

    Update UserForm.php

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Filament\Resources\Users\Schemas;
 
 use App\Filament\Resources\Users\Pages\CreateUser;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 use Illuminate\Support\Facades\Hash;
@@ -16,22 +17,10 @@ final class UserForm
     {
         return $schema
             ->components([
+                Select::make('type')
+                    ->searchable(),
                 TextInput::make('name')
-                    ->maxLength(255)
-                    ->required(),
-                TextInput::make('email')
-                    ->maxLength(255)
-                    ->unique()
-                    ->email()
-                    ->required(),
-                TextInput::make('password')
-                    ->password()
-                    ->required(fn ($livewire): bool => $livewire instanceof CreateUser)
-                    ->revealable(filament()->arePasswordsRevealable())
-                    ->rule(Password::default())
-                    ->autocomplete('new-password')
-                    ->dehydrated(fn ($state): bool => filled($state))
-                    ->dehydrateStateUsing(fn ($state): string => Hash::make($state)),
+                    ->afterStateUpdatedJs("\$set('type', \$state)")
             ]);
     }
 }
