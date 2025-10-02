8dd9c49fc91f6b6d500e9dfc55c594db74b7158f
andyov17 <andrew.ovens@hotmail.com>
2025-10-02 11:59:57 +0100

    a

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -18,7 +18,7 @@ final class UserForm
         return $schema
             ->components([
                 Select::make('type')
-                    ->searchable(),
+                    ->native(false),
                 TextInput::make('name')
                     ->afterStateUpdatedJs("\$set('type', \$state)")
             ]);
